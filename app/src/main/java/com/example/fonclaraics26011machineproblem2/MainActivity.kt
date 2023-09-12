9dc0949248c6e53bda5a5b41feb26b4a4af7fb48
melchongx <melfredfonclara@gmail.com>
2023-09-12 11:08:21 +0800

    Hello from the other side

--- app/src/main/java/com/example/fonclaraics26011machineproblem2/MainActivity.kt
+++ app/src/main/java/com/example/fonclaraics26011machineproblem2/MainActivity.kt
@@ -2,10 +2,38 @@ package com.example.fonclaraics26011machineproblem2
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+    override fun onStart() {
+        super.onStart()
+        Toast.makeText(this, "OnStart", Toast.LENGTH_SHORT).show();
+        Log.i("info", "OnStart");
+    }
+    override fun onPause() {
+        super.onPause()
+        Toast.makeText(this, "OnPause", Toast.LENGTH_SHORT).show();
+        Log.i("info", "OnPause");
+    }
+    override fun onStop() {
+        super.onStop()
+        Toast.makeText(this, "OnStop", Toast.LENGTH_SHORT).show();
+        Log.i("info", "OnStop");
+    }
+    override fun onRestart() {
+        super.onRestart()
+        Toast.makeText(this, "OnRestart", Toast.LENGTH_SHORT).show();
+        Log.i("info", "OnRestart");
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Toast.makeText(this, "OnDestroy", Toast.LENGTH_SHORT).show();
+        Log.i("info", "OnDestroy");
     }
 }
